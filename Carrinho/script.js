2dd7c41da7fd516cf923d0cb9fd331d7dde643ac
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-03-14 16:55:22 -0300

    ShowerRoom

---
+++ Carrinho/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+    // Recupera o carrinho do localStorage
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  
+    // Elemento onde a lista será exibida
+    const listaElement = $('#lista');
+  
+    // Elemento para exibir o total em preço
+    const totalElement = $('#total');
+  
+    // Função para exibir o carrinho
+    function exibirCarrinho() {
+
+      // Limpa o conteúdo atual da lista
+      listaElement.empty();
+  
+      // Variável para calcular o total em preço
+      let totalPreco = 0;
+  
+      // Itera sobre os itens do carrinho
+      $.each(carrinho, function (index, item) {
+
+        // Cria um elemento de lista para cada item
+        const listItem = $("<p>").text(`${item.descricao} - Preço: $${item.preco}`
+        );
+  
+        const removeButton = $("<button>").text("-").css("margin-left", "10px").click(function() {
+            removerItemDoCarrinho(index);
+          });
+  
+        listItem.append(removeButton);
+        listaElement.append(listItem);
+  
+        totalPreco += item.preco;
+
+      });
+  
+      // Exibe o total em preço no elemento totalElement
+      totalElement.text(`Total: $${totalPreco.toFixed(2)}`);
+    }
+  
+    // Função para remover um item do carrinho
+    function removerItemDoCarrinho(index) {
+      carrinho.splice(index, 1);
+      localStorage.setItem("carrinho", JSON.stringify(carrinho));
+      exibirCarrinho();
+    }
+  
+    // Chama a função para exibir o carrinho
+    exibirCarrinho();
+  });
+  
+  function gerarDocumentoWord() {
+    const listaElement = document.getElementById("lista");
+    const totalElement = document.getElementById("total");
+  
+    // Clona a lista para evitar a modificação direta na lista original
+    const listaClone = listaElement.cloneNode(true);
+  
+    // Remove os botões de remoção da lista clonada
+    $(listaClone).find("button").remove();
+  
+    const listaHtml = listaClone.innerHTML;
+    const totalHtml = totalElement.innerHTML;
+  
+    const conteudoHtml = `
+      <html>
+        <head>
+          <meta charset="UTF-8" />
+        </head>
+        <body>
+          <h1>Pedido confirmado!</h1>
+          <h3>Agradecemos sua preferência</h3>
+          ${listaHtml}
+          <br>
+          <br>
+          ${totalHtml}
+        </body>
+      </html>
+    `;
+  
+    const blob = new Blob([conteudoHtml], { type: "application/msword" });
+    const link = document.createElement("a");
+  
+    link.href = URL.createObjectURL(blob);
+    link.download = "carrinho.doc";
+    link.click();
+    document.getElementById("pedido").style.display = "block";
+  }
+  
+  function successClose() {
+    document.getElementById("pedido").style.display = "none";
+  }
